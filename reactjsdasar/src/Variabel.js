ea8a178f46d390873abbcdda6838b8b88efec9f8
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-11-01 21:26:49 +0700

    const var let

---
+++ reactjsdasar/src/Variabel.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+// Constants
+//const marvel = 5000;
+//marvel = 3000;
+
+// Var
+// var marvel = 70000;
+//     if (true) {
+//         var marvel = 1000;
+//     }
+    
+// Let
+let marvel = 70000;
+    if (true) {
+        let marvel = 200;
+    }
+    
+const Variabel = () => {
+    return (
+    <div>
+        <h2> Avengers Variabel : {marvel}</h2>
+    </div>
+    )
+}
+export default Variabel
