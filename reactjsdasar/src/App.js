d4cb6634f7ac06bf970afe015bd398cf47f0d32a
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-11-01 21:08:01 +0700

    component

---
+++ reactjsdasar/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h2>Hello world</h2>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjsdasar/src/Navbar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+
+// Class Component
+export default class Navbar extends Component {
+    render() {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )}
+}
+
+// functions component
+
+/*export default function Navbar() {
+    return (
+    <div>
+        <h2>Navbar</h2>
+    </div>
+    )
+}
+*/
+
+// Arrow Functions
+/*
+import React from 'react'
+
+const Navbar = () => {
+    return (
+    <div>
+        <h2>Navbar</h2>
+    </div>
+    )
+}
+export default Navbar
+*/
