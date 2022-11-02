8cb82a6b388bb59a2bd5bf5dac7af3f881fe7163
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-11-02 14:27:44 +0700

    CRUD Dasar

--- reactjsdasar/src/App.js
+++ reactjsdasar/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Header from './react/Header';
 import Navbar from './Navbar';
 
 
--- reactjsdasar/src/index.js
+++ reactjsdasar/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import App from './App';
-//import Variabel from './Variabel';
-//import StateProps from './StateProps'
-//import Map from './Map'
-import Lifecycle from './Lifecycle';
+//import Variabel from './react/Variabel';
+//import StateProps from './react/StateProps'
+//import Map from './react/Map'
+//import Lifecycle from './react/Lifecycle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Crud  from './crud/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Lifecycle />
+    <Crud />
   </React.StrictMode>
 );
 
